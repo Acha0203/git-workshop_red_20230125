7503a3889489a6abb98074c3f456ce3e7c9da40a
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-01-25 22:01:09 +0900

    chore: handleClick() から cpuAction() を切り離してみる

--- game/src/components/Game.jsx
+++ game/src/components/Game.jsx
@@ -1,102 +1,126 @@
-import React, { useState } from "react";
-import Board from "./Board";
+import React, { useEffect, useState, useCallback } from 'react';
+import Board from './Board';
 
 const Game = () => {
   const [history, setHistory] = useState([
     {
-        squares: Array(9).fill(null)
-    }
+      squares: Array(9).fill(null),
+    },
   ]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [moves, setMoves] = useState(true);
+  const [disabledClick, setDisabledClick] = useState(false);
   const [playCount, setPlayCount] = useState(0);
+  const [playersTurn, setPlayersTurn] = useState(true);
+  const [currentSquares, setCurrentSquares] = useState([]);
 
-  const MAX_PLAY_COUNT = 9;
+  const MAX_PLAY_COUNT = 5;
+
+  useEffect(() => {
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move # ${move}` : `Go to game start`;
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}> {desc} </button>
+        </li>
+      );
+    });
+    setMoves(moves);
+  }, [history]);
 
   /**
    * マス目クリック時
    * @param {int} index
    */
   const handleClick = (index) => {
-    const historyCurrent = history.slice(0, playCount +1);
-    const current = historyCurrent[historyCurrent.length -1];
+    setDisabledClick(true);
+
+    const historyCurrent = history.slice(0, playCount + 1);
+    const current = historyCurrent[historyCurrent.length - 1];
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[index]) {
       return;
     }
-    squares[index] = xIsNext ? "X" : "O";
-    
+    squares[index] = xIsNext ? 'X' : 'O';
+
     setPlayCount(historyCurrent.length);
-    setHistory([...historyCurrent, {squares}]);
-    setXIsNext(!xIsNext);
+    setHistory([...historyCurrent, { squares }]);
+    setPlayersTurn(false);
+    setCurrentSquares(squares);
+
+    // setTimeout(() => {
+    // cpuAction(squares);
+    // setDisabledClick(false);
+    // }, 1000);
   };
 
   /**
-   * タイムトラベルボタン
+   * タイムトラベル
    */
   const jumpTo = (step) => {
-    if (step === 0){addHidden()};
     setPlayCount(step);
     setXIsNext(step % 2 === 0);
   };
 
-  const moves = history?.map((step, move) => {
-    const desc = move ? `Go to # ${move}` : `Restart`;
-    let visibility = "";
-    let restart = "";
+  const cpuAction = useCallback(() => {
+    if (calculateWinner(currentSquares)) return;
+    const currentHistory = history.slice(0, playCount + 2);
 
-    if (move === 0){
-      restart = "restart";
-    }else visibility = "hidden";
+    const possible_hands = [];
+    let hand = currentSquares.indexOf(null);
+    while (hand !== -1) {
+      possible_hands.push(hand);
+      hand = currentSquares.indexOf(null, hand + 1);
+    }
 
-    return (
-      <li key={move}
-        className={visibility}
-      >
-        <button 
-          id={restart}  
-          onClick={() => jumpTo(move)}
-        > 
-          {desc} 
-        </button>
-      </li>
-    );
-  });
+    if (possible_hands.length === 0) return;
+
+    const action_hand =
+      possible_hands[Math.floor(Math.random() * possible_hands.length)];
+    const cpuStatus = !xIsNext;
+    currentSquares[action_hand] = cpuStatus ? 'X' : 'O';
+    currentHistory[history.length - 1].squares = currentSquares;
+
+    setHistory([...currentHistory, { currentSquares }]);
+    // console.log(history);
+    setPlayersTurn(true);
+    setXIsNext(xIsNext);
+  }, [currentSquares, history, playCount, xIsNext]);
+
+  useEffect(() => {
+    console.log(history);
+
+    if (!playersTurn) {
+      // setTimeout(() => {
+      cpuAction();
+      setDisabledClick(false);
+      // }, 1000);
+    }
+  }, [cpuAction, history, playCount, playersTurn]);
 
   /**
-   * 勝敗が決したのち、タイムトラベルボタンを表示
-   * Restartするとタイムトラベルボタンを非表示
+   * 勝者/次のプレイヤーを表示
+   * @returns {string}
    */
-  const removeHidden = () => {
-    const buttonList = document.getElementById("buttonList");
-    const children = buttonList.children;
-    for (let i = 0; i < children.length; i++){
-      children[i].classList.remove('hidden');
-      console.log(children[i]);
-    };
+  const getWinner = () => {
+    const winner = calculateWinner(history[playCount].squares);
+    if (winner) {
+      return '勝者: ' + winner;
+    } else if (playCount === MAX_PLAY_COUNT) {
+      return '引き分けです';
+    } else {
+      return '次のプレイヤー: ' + (xIsNext ? 'あなた' : 'CPU');
+    }
   };
-    
-  const addHidden = () => {
-    const buttonList = document.getElementById("buttonList");
-    const children = buttonList.children;
-    for (let i = 0; i < children.length; i++){
-      children[i].classList.add('hidden');
-      console.log(children[i]);
-    };
-  }
 
-  /**
-   * 現在の盤面
-   */
-  const current = history[playCount];
-  
   /**
    * 勝敗を計算する
    * @param {array} resultSquares
    * @returns {string|null}
-  */
- const calculateWinner = (resultSquares) => {
-   const lines = [
+   */
+  const calculateWinner = (resultSquares) => {
+    const lines = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -106,52 +130,46 @@ const Game = () => {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    
+
     for (let i = 0; i < lines.length; i++) {
-     const [a, b, c] = lines[i];
-     if (
-       resultSquares[a] &&
-       resultSquares[a] === resultSquares[b] &&
-       resultSquares[a] === resultSquares[c]
-       ) {
-         return [a, b ,c];
-        }
+      const [a, b, c] = lines[i];
+      if (
+        resultSquares[a] &&
+        resultSquares[a] === resultSquares[b] &&
+        resultSquares[a] === resultSquares[c]
+      ) {
+        return resultSquares[a];
+      }
     }
     return null;
   };
-  
-  /**
-   * 勝者/次のプレイヤーを表示
-   * @returns {string}
-   */
-  const winner = calculateWinner(current.squares)
-  console.log(winner);
-  let result = "";
-  if (winner) {
-    removeHidden();
-    result = "勝者: " + current.squares[winner[0]];
-  } else if (playCount === MAX_PLAY_COUNT) {
-    removeHidden();
-    result = "引き分けです";
-  } else {
-    result = "次のプレイヤー: " + (xIsNext ? "X" : "O");
-  };
-     
+
+  const current = history[playCount];
+
+  // 全てを初期化する
+  //const resetAll = () => {
+  //  setHistory(
+  //      {
+  //          squares: Array(9).fill(null)
+  //      }
+  //  );
+  //  setXIsNext(true);
+  //};
+
   return (
-    <div className="game">
+    <div className={'game ' + (disabledClick ? 'disabled' : '')}>
       <div className="game-board">
-        <Board 
-          winnerLines={winner} 
-          squares={current.squares} 
-          onClick={index => handleClick(index)} 
+        <Board
+          squares={current.squares}
+          onClick={(index) => handleClick(index)}
         />
       </div>
       <div className="game-info">
-          <div>{result}</div>
-          <ul id="buttonList">{moves}</ul>
+        <div>{getWinner()}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
-  )
+  );
 };
 
 export default Game;
